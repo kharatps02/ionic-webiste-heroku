8df9e6f14b2abd81e1e1341b057c80cfc50b1de0
Popat Kharat <kharatps02@gmail.com>
2018-02-16 18:46:10 +0530

    - changed to qa

--- src/shared/environment.ts
+++ src/shared/environment.ts
@@ -1,10 +1,10 @@
 export var ENVIRONMENT = {
-    APP_BASE_URL: 'https://rezility-dev.herokuapp.com',
-    APP_VERSION: '1.8.0',
-    APP_VERSION_DISPLAY: 'v1.8.0 (DEV)',
+    APP_BASE_URL: 'https://rezility-qa.herokuapp.com',
+    APP_VERSION: '1.7.2 ',
+    APP_VERSION_DISPLAY: '1.7.2 (QA-127)',
     PUBNUB_KEYS: {
         PUBLISH_KEY: 'pub-c-be48fa19-969b-4619-82c7-f1c3e40eaef3',
         SUBSCRIBE_KEY: 'sub-c-45fc1ec6-357e-11e7-887b-02ee2ddab7fe'
     },
-    GOOGLE_ANALYTICS_TRACKING_ID: 'UA-98874270-1',    
+    GOOGLE_ANALYTICS_TRACKING_ID: 'UA-98874270-1',
 }
--- www/build/main.js
+++ www/build/main.js
@@ -6455,9 +6455,9 @@ NewMessage = __decorate([
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return ENVIRONMENT; });
 var ENVIRONMENT = {
-    APP_BASE_URL: 'https://rezility-dev.herokuapp.com',
-    APP_VERSION: '1.8.0',
-    APP_VERSION_DISPLAY: 'v1.8.0 (DEV)',
+    APP_BASE_URL: 'https://rezility-qa.herokuapp.com',
+    APP_VERSION: '1.7.2 ',
+    APP_VERSION_DISPLAY: '1.7.2 (QA-127)',
     PUBNUB_KEYS: {
         PUBLISH_KEY: 'pub-c-be48fa19-969b-4619-82c7-f1c3e40eaef3',
         SUBSCRIBE_KEY: 'sub-c-45fc1ec6-357e-11e7-887b-02ee2ddab7fe'
