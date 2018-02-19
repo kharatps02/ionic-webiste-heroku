b58c52b9b5e7d68b578a0abb77804a0e0044691a
Popat Kharat <kharatps02@gmail.com>
2018-02-19 15:05:28 +0530

    - updated version

--- src/shared/environment.ts
+++ src/shared/environment.ts
@@ -1,7 +1,7 @@
 export var ENVIRONMENT = {
     APP_BASE_URL: 'https://rezility-api-uat.herokuapp.com',   
-    APP_VERSION: '1.7.2',
-    APP_VERSION_DISPLAY: '1.7.2 (UAT-127)',
+    APP_VERSION: '1.8.4',
+    APP_VERSION_DISPLAY: '1.8.4',
     PUBNUB_KEYS: {
         PUBLISH_KEY: 'pub-c-b7a78aea-92b8-45c8-856c-bd85feaf25a4',
         SUBSCRIBE_KEY: 'sub-c-3b9bb1f2-42c7-11e7-b847-0619f8945a4f'
